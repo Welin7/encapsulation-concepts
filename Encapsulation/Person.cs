a94347829fe130fc78a1980caedd5ddf8822f172
Welington Dias <33883532+Welin7@users.noreply.github.com>
2022-03-01 01:38:04 -0300

    Encapsulation.
    Using the encapsulation concept to protect the business rule.

---
+++ Encapsulation/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation
+{
+    public class Person
+    {
+        public Person(string name, int mass, double hight)
+        {
+            this.Name = name;
+            this.Mass = mass;
+            this.Hight = hight;
+        }
+        
+        public string? Name { get; private set; }
+        public int Mass {get; private set;}
+        public double Hight { get; private set;}
+
+        //Access via property public get
+        public double IMC => CalculeIMC();
+
+        //Business rule hide for modifier private
+        private double CalculeIMC()
+        {
+          return this.Mass / (Math.Pow(this.Hight, 2));
+        }
+        public override string ToString()
+        {
+            return $"{this.Name} - {this.Mass} - {this.Hight}";
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Apply encapsulation concepts");
+
+var person = new Encapsulation.Person("João Guilherme da Silva", 65, 1.66);
+
+//Access via property public get
+var imc = person.IMC;
+
+Console.WriteLine();
+Console.WriteLine(person);
+Console.WriteLine();
+Console.WriteLine($"The IMC of {person.Name} is of {imc}");
+Console.ReadLine();
